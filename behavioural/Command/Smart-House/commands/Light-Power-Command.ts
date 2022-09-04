108051f295e860c7598b08da3ce63dbe1a6a61d3
taiury <alexandretaiury@gmail.com>
2022-09-04 13:49:21 -0300

    added: pattern command

--- behavioural/Command/Smart-House/commands/Light-Power-Command.ts
+++ behavioural/Command/Smart-House/commands/Light-Power-Command.ts
@@ -1,5 +1,5 @@
+import { SmartHouseLight } from "../Smart-House-Light";
 import { SmartHauseCommand } from "./Smart-House-Command";
-import { SmartHouseLight } from "./Smart-House-Light";
 
 export class LightPowerCommand implements SmartHauseCommand {
   constructor(private readonly Light: SmartHouseLight) {}
